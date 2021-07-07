460efd592a140e615756d30ac0bd9d498d352ab6
Sergey Egorov <me@egorov.space>
2021-07-07 14:24:15 +0500

    Черновик метода генерации формы для отправки запроса на авторизацию Google

--- src/makeGoogleOAuthTokenRequestForm.js
+++ src/makeGoogleOAuthTokenRequestForm.js
@@ -1,5 +1,24 @@
-function makeGoogleOAuthTokenRequestForm() {
+import { googleAuthSettings } from './googleAuthSettings';
 
+function makeGoogleOAuthTokenRequestForm(document) {
+
+  const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+  const form = document.createElement('form');
+
+  form.setAttribute('method', 'GET');
+  form.setAttribute('action', oauth2Endpoint);
+
+  for(const property in googleAuthSettings) {
+
+    const input = document.createElement('input');
+
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', property);
+    input.setAttribute('value', googleAuthSettings[property]);
+    form.appendChild(input);
+  }
+
+  return form;
 }
 
 export { makeGoogleOAuthTokenRequestForm };
--- src/makeGoogleOAuthTokenRequestForm.test.js
+++ src/makeGoogleOAuthTokenRequestForm.test.js
@@ -1,6 +1,27 @@
+import { googleAuthSettings } from './googleAuthSettings';
 import { makeGoogleOAuthTokenRequestForm } from './makeGoogleOAuthTokenRequestForm';
 
-it('should pass', () => {
+test('should pass', () => {
 
-  const form = makeGoogleOAuthTokenRequestForm();
+  const form = makeGoogleOAuthTokenRequestForm(document);
+
+  expect(form.getAttribute('method')).toEqual('GET');
+  expect(form.getAttribute('action')).toEqual('https://accounts.google.com/o/oauth2/v2/auth');
+  expect(form.elements.length).toEqual(5);
+
+  for(const element of form.elements) {
+
+    expect(element.tagName).toEqual('INPUT');
+    expect(element.getAttribute('type')).toEqual('hidden');
+
+    expect(
+      Object.keys(googleAuthSettings).indexOf(element.getAttribute('name'))
+    )
+    .toBeGreaterThan(-1);
+
+    expect(
+      element.getAttribute('value')
+    )
+    .toEqual(googleAuthSettings[element.getAttribute('name')]);
+  }
 });
